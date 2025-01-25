b4bb73c907b9d66ddc07b33d849c9662d947de7d
adrien-force <force.adri@gmail.com>
2025-01-25 13:12:24 +0100

    feat: welcome partial + product grid partial

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,7 +10,7 @@ final class HomeController extends AbstractController{
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('home/homeIndex.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
