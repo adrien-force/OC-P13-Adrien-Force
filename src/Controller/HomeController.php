8834f1c3c9e870ad561f67f8d55fee8d1ef4d3bd
adrien-force <force.adri@gmail.com>
2025-02-04 20:22:05 +0100

    feat: login page skeleton

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -20,6 +24,34 @@ final class HomeController extends AbstractController{
     {
         return $this->render('productPage/product.html.twig', [
             'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/login', name: 'app_login')]
+    public function login(Request $request): Response
+    {
+        $form = $this->createFormBuilder()
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email',
+                'attr' => ['placeholder' => 'Entrez votre email']
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'attr' => ['placeholder' => 'Entrez votre mot de passe']
+            ])
+            ->add('submit', SubmitType::class, ['label' => 'Se connecter'])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $this->addFlash('success', 'Tentative de connexion avec : ' . $data['email']);
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('login/loginPage.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
