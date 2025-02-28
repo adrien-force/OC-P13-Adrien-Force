f528f8d4d0812c7b31b7b499362a2515eef4f75b
adrien-force <force.adri@gmail.com>
2025-02-28 20:00:09 +0100

    feat: integrate JWT authentication and API documentation with Nelmio and Lexik bundles

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,9 @@ use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+use Nelmio\ApiDocBundle\Attribute\Model;
+use Nelmio\ApiDocBundle\Attribute\Security;
+use OpenApi\Attributes as OA;
 final class HomeController extends AbstractController{
     #[Route('/home', name: 'app_home')]
     public function index(
@@ -19,5 +21,21 @@ final class HomeController extends AbstractController{
             'controller_name' => 'HomeController',
             'products' => $products,
         ]);
+    }
+
+
+    #[Route('/api/test/{name}', name: 'api_test', methods: ['GET'])]
+    #[OA\Response(
+        response: 200,
+        description: "Returns the test response",
+        content: new OA\JsonContent(
+            type: "string",
+            example: "Hello World"
+        )
+    )]
+    #[Security(name: 'Bearer')]
+    public function apiTestRoute(string $name): Response
+    {
+        return new Response('Hello ' . $name);
     }
 }
