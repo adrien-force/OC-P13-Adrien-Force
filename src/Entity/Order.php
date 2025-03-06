896fa54ce0b201a03dc92fb85dabc8a74363c19d
adrien-force <force.adri@gmail.com>
2025-03-06 22:36:13 +0100

    refactor: add PHPStan ignore directive for id property in Order entity

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -18,6 +18,7 @@ class Order
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    /** @phpstan-ignore-next-line */
     private ?int $id = null;
 
     #[ORM\ManyToOne(inversedBy: 'order')]
