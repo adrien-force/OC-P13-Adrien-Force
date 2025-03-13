b8a286f00151b31005747c0009451725182624dd
adrien-force <force.adri@gmail.com>
2025-03-13 19:07:52 +0100

    feat: add redirect route to home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -19,5 +20,11 @@ final class HomeController extends AbstractController
             'controller_name' => 'HomeController',
             'products' => $products,
         ]);
+    }
+
+    #[Route('/', name: 'app_root')]
+    public function redirectToHome(): RedirectResponse
+    {
+        return $this->redirectToRoute('app_home');
     }
 }
